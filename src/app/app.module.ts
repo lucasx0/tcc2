1d3f8862ebcfc78d53620588bf5b94c59613544e
Lucas Almeida <lucas.almeidasilva49@gmail.com>
2020-11-16 17:15:53 -0300

    projeto novo reformulado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabecalhoComponent } from './cabecalho/cabecalho.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContatoComponent } from './contato/contato.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { FaleconoscoComponent } from './faleconosco/faleconosco.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabecalhoComponent,
+    HomeComponent,
+    FooterComponent,
+    ContatoComponent,
+    CadastroComponent,
+    FaleconoscoComponent,
+    
   ],
   imports: [
     BrowserModule,
